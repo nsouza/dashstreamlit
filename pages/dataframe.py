b5daa2bdbe24ec6a66283dcbd9931cbfa51e1c2c
Nilo <nilo.contatos@gmail.com>
2026-01-25 16:54:38 -0300

    usar expander

--- pages/dataframe.py
+++ pages/dataframe.py
@@ -2,4 +2,34 @@ import streamlit as st
 from dataset import df
 
 st.title('Dataset de Vendas')
+with st.expander('Colunas'):
+    colunas = st.multiselect(
+        'Selecione as Colunas',
+        list(df.columns),
+        list(df.columns)
+    )
+    
+st.sidebar.title('Filtros')
+with st.sidebar.expander('Categoria do Produto'):
+    categorias = st.multiselect(
+        'Selecione as categorias',
+        df['Categoria do Produto'].unique(),
+        df['Categoria do Produto'].unique(),        
+    )
+    
+with st.sidebar.expander('Preço do Produto'):
+    preco = st.slider(
+        'Selecione o Preço',
+        0,5000,
+        (0,5000)               
+    )
+    
+with st.sidebar.expander('Data da Compra'):
+    data_compra = st.date_input(
+        'Selecione a data',
+        (df['Data da Compra'].min(),
+        df['Data da Compra'].max())              
+    ) 
+    
 st.dataframe(df)
+
